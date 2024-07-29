de0fb9dfcf5663fd76d3aae608b3119214197f47
victordmontero <victordanielmontero@gmail.com>
2024-07-29 22:37:36 +0200

    Changed testing framework to gtest and updated demo code

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <assert.h>
-#include <sqlite3_orm.hpp>
-#include <log.hpp>
 #include <vector>
 #include <map>
+#include "log.hpp"
+#include "sqlite3_orm.hpp"
+#include "string.hpp"
 
 void debug(const char* msg)
 {
@@ -17,59 +18,103 @@ int main(int args, char* argv[])
 	sqlite3_orm::Log::setLogDebugCallback(debug);
 	sqlite3_orm::Log::setLogErrorCallback(debug);
 
-	sqlite3_orm::Database database("softpay.sqlite3");
+	//Database creation
+	sqlite3_orm::Database database("contacts.sqlite3");
 	database.Open();
 
-	sqlite3_orm::TableResult tableResult = database.SelectTableAll("SP_TERMINALS");
+	sqlite3_orm::PrimaryKeyConstraint primaryKeyColumnConstraint("", true, true);
+	sqlite3_orm::UniqueConstraint contactNameColumnConstraint;
+	sqlite3_orm::ColumnConstraint contactPhoneColumnConstraint;
 
+	//Table creation
+
+	std::string createTableStmtSqlStr = "CREATE TABLE CONTACTS(";
+
+	createTableStmtSqlStr += "Id INTEGER ";
+	createTableStmtSqlStr += primaryKeyColumnConstraint.parse();
+	createTableStmtSqlStr += ",";
+	createTableStmtSqlStr += "ContactName VARCHAR(20) ";
+	createTableStmtSqlStr += contactNameColumnConstraint.parse();
+	createTableStmtSqlStr += ",";
+	createTableStmtSqlStr += "ContactPhone VARCHAR(14) ";
+	createTableStmtSqlStr += contactPhoneColumnConstraint.parse();
+
+	createTableStmtSqlStr += ")";
+
+	database.ExecuteSQL(createTableStmtSqlStr.c_str());
+
+	//Inserting a row
+	std::map<std::string, std::string> columnNames;
+	columnNames["ContactName"] = sqlite3_orm::string("Diana").ToSqlString();
+	columnNames["ContactPhone"] = sqlite3_orm::string("020 7123 4567").ToSqlString();
+
+	//Beginning a transaction to ensure atomic transactions
+	database.BeginTran();
+	rowsChanged = database.InsertIntoTable("CONTACTS", columnNames);
+	database.Commit();
+	printf("rowsChanged = %ld\n", rowsChanged);
+
+	//Selecting from CONTACTS table
+	sqlite3_orm::TableResult tableResult = database.SelectTableAll("CONTACTS");
 	tableResult.firstRow();
 	printf("Rows=[%d]\n", tableResult.getRowCount());
 
 	for (int i = 0; i < tableResult.getRowCount(); i++)
 	{
-		printf("%s:%s\n", tableResult["TerminalId"].c_str(),
-			tableResult["TerminalModelName"].c_str());
+		printf("%s\t%s\t%s\n", tableResult["Id"].c_str(),
+			tableResult["ContactName"].c_str(), tableResult["ContactPhone"].c_str());
 		tableResult.nextRow();
 	}
 
 	tableResult.lastRow();
 
-	printf("TerminalId = %s\n", tableResult["TerminalId"].c_str());
-
+	//Updating a row
 	database.BeginTran();
 
 	std::map<std::string, std::string> updateColumnNames;
-	updateColumnNames["Mobile"] = "1";
-	updateColumnNames["Color"] = "1";
-	updateColumnNames["BuiltInPrinter"] = "0";
+	//updateColumnNames["Id"] = "1";
+	updateColumnNames["ContactName"] = sqlite3_orm::string("Meghan").ToSqlString();
+	updateColumnNames["ContactPhone"] = sqlite3_orm::string("020 3217 7654").ToSqlString();
 
 	rowsChanged = database.UpdateTable(
-		"SP_TERMINALS",
-		(char*)(std::string("TerminalId = ") + tableResult["TerminalId"]).c_str(),
+		"CONTACTS",
+		(char*)(std::string("Id = ") + tableResult["Id"]).c_str(),
 		updateColumnNames);
 
 	printf("rowsChanged = %ld\n", rowsChanged);
 
-
-	tableResult.previousRow();
-	std::map<std::string, std::string> updateColumnNames2;
-	updateColumnNames2["Mobile"] = "1";
-	updateColumnNames2["Color"] = "1";
-	updateColumnNames2["BuiltInPrinter"] = "0";
-
-	rowsChanged = database.UpdateTable(
-		"SP_TERMINALS",
-		(char*)(std::string("TerminalId = ") + tableResult["TerminalId"]).c_str(),
-		updateColumnNames2);
-
-	printf("rowsChanged = %ld\n", rowsChanged);
-
 	database.Commit();
+
+	//Selecting again
+	tableResult = database.SelectTableAll("CONTACTS");
+	tableResult.firstRow();
+	printf("Rows=[%d]\n", tableResult.getRowCount());
+
+	for (int i = 0; i < tableResult.getRowCount(); i++)
+	{
+		printf("%s\t%s\t%s\n", tableResult["Id"].c_str(),
+			tableResult["ContactName"].c_str(), tableResult["ContactPhone"].c_str());
+		tableResult.nextRow();
+	}
+
+	tableResult.lastRow();
+
+	//Inserting another row
+	columnNames["ContactName"] = sqlite3_orm::string("Diana").ToSqlString();
+	columnNames["ContactPhone"] = sqlite3_orm::string("020 7123 4567").ToSqlString();
+
+	//Beginning a transaction to ensure atomic transactions
 	database.BeginTran();
-
-	rowsChanged = database.DeleteRow("SP_TERMINALS");
+	rowsChanged = database.InsertIntoTable("CONTACTS", columnNames);
+	database.Commit();
 	printf("rowsChanged = %ld\n", rowsChanged);
 
+	//Deleting all rows
+	database.BeginTran();
+	rowsChanged = database.DeleteRow("CONTACTS");
+	printf("rowsChanged = %ld\n", rowsChanged);
 	database.Commit();
+
 	database.Close();
+
 }
